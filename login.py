f5f378833faaad941bff905a83537cddc6cdf44d
Abhishek Manvendra Singh <abhisinghp13@gmail.com>
2023-04-04 23:37:39 +0530

    login page using python

---
+++ login.py
@@ -0,0 +1,21 @@
+from tkinter import *
+from tkinter import ttk
+from PIL import Image,ImageTk
+
+
+root = Tk()
+root.title("Login")
+root.geometry("1500x800")
+
+frame=Frame(root,bg="black")
+frame.place(x=600, y=170, width=350, height=500)
+
+
+root.mainloop()
+
+
+
+
+
+
+
